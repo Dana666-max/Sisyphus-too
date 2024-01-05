e92a90c8be3f191f7a001489d328dec7426143d0
Dana <danamiz2006@gmail.com>
2024-01-04 20:52:13 -0800

    Initial Commit

---
+++ Sisyphus too/Sisyphus_tooApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sisyphus_tooApp.swift
+//  Sisyphus too
+//
+//  Created by student1 on 2024-01-04.
+//
+
+import SwiftUI
+
+@main
+struct Sisyphus_tooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
